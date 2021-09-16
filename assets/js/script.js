b20590e61fe1ff3e8e9181a8b9ca4b8c7a47e207
Kurdiamina3 <kurdiamina3@gmail.com>
2021-09-16 16:11:04 -0400

    planner but cant save

---
+++ assets/js/script.js
@@ -0,0 +1,333 @@
+
+
+var objToday = new Date(),
+weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+dayOfWeek = weekday[objToday.getDay()],
+domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+curMonth = months[objToday.getMonth()],
+curYear = objToday.getFullYear(),
+curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+
+$(document).ready(function () {
+  window.onload = function(){
+    colorCode();
+    nineAM();
+    tenAM();
+    elevenAM();
+    twelvePM();
+    onePM();
+    twoPM();
+    threePM();
+    fourPM();
+    fivePM();
+    sixPM();
+   }
+
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));  
+  document.getElementsById('currentDay')[0].textContent = today;
+
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    
+    function colorCode() {
+      if (now > 9) {
+        $("#comment9am").addClass("past");
+      } else if (now >= 9 && now < 10) {
+        $("#comment9am").addClass("present");
+      } else if (now < 9) {
+        $("#comment9am").addClass("future");
+      }
+      if (now > 10) {
+        $("#comment10am").addClass("past");
+      } else if (now >= 10 && now < 11) {
+        $("#comment10am").addClass("present");
+      } else if (now < 10) {
+        $("#comment10am").addClass("future");
+      }
+      if (now > 11) {
+        $("#comment11am").addClass("past");
+      } else if (now >= 11 && now < 12) {
+        $("#comment11am").addClass("present");
+      } else if (now < 11) {
+        $("#comment11am").addClass("future");
+      }
+      if (now > 12) {
+        $("#comment12pm").addClass("past");
+      } else if (now >= 12 && now < 13) {
+        $("#comment12pm").addClass("present");
+      } else if (now < 12) {
+        $("#comment12pm").addClass("future");
+      }
+      if (now > 13) {
+        $("#comment1pm").addClass("past");
+      } else if (now >= 13 && now < 14) {
+        $("#comment1pm").addClass("present");
+      } else if (now < 13) {
+        $("#comment1pm").addClass("future");
+      }
+      if (now > 14) {
+        $("#comment2pm").addClass("past");
+      } else if (now >= 14 && now < 15) {
+        $("#comment2pm").addClass("present");
+      } else if (now < 14) {
+        $("#comment2pm").addClass("future");
+      }
+      if (now > 15) {
+        $("#comment3pm").addClass("past");
+      } else if (now >= 15 && now < 16) {
+        $("#comment3pm").addClass("present");
+      } else if (now < 15) {
+        $("#comment3pm").addClass("future");
+      }
+      if (now > 16) {
+        $("#comment4pm").addClass("past");
+      } else if (now >= 16 && now < 17) {
+        $("#comment4pm").addClass("present");
+      } else if (now < 16) {
+        $("#comment4pm").addClass("future");
+      }
+      if (now > 17) {
+        $("#comment5pm").addClass("past");
+      } else if (now >= 17 && now < 18) {
+        $("#comment5pm").addClass("present");
+      } else if (now < 17) {
+        $("#comment5pm").addClass("future");
+      }
+      if (now > 18) {
+        $("#comment5pm").addClass("past");
+      } else if (now >= 18 && now < 19) {
+        $("#comment5pm").addClass("present");
+      } else if (now < 18) {
+        $("#comment5pm").addClass("future");
+      }
+
+    }    
+
+    function nineAM() {
+      var input_textarea = document.querySelector("#comment9am");
+      var output_div = document.querySelector("#comment9am");
+      var save_button = document.querySelector("#button9am");
+    
+      save_button.addEventListener("click", updateOutput);
+    
+      output_div.textContent = localStorage.getItem("content");
+      input_textarea.value = localStorage.getItem("content");
+    
+      function updateOutput() {
+        localStorage.setItem("content", input_textarea.value);
+    
+        output_div.textContent = input_textarea.value;
+      }
+    }
+    
+    
+
+    function tenAM() {
+      var input_textarea2 = document.querySelector("#comment10am");
+      var output_div2 = document.querySelector("#comment10am");
+      var save_button2 = document.querySelector("#button10am");
+    
+      save_button2.addEventListener("click", updateOutput2);
+    
+      output_div2.textContent = localStorage.getItem("content2");
+      input_textarea2.value = localStorage.getItem("content2");
+    
+      function updateOutput2() {
+        localStorage.setItem("content2", input_textarea2.value);
+    
+        output_div2.textContent = input_textarea2.value;
+      }
+    }
+    
+    
+    function elevenAM() {
+      var input_textarea3 = document.querySelector("#comment11am");
+      var output_div3 = document.querySelector("#comment11am");
+      var save_button3 = document.querySelector("#button11am");
+    
+      save_button3.addEventListener("click", updateOutput3);
+    
+      output_div3.textContent = localStorage.getItem("content3");
+      input_textarea3.value = localStorage.getItem("content3");
+    
+      function updateOutput3() {
+        localStorage.setItem("content3", input_textarea3.value);
+    
+        output_div3.textContent = input_textarea3.value;
+      }
+    }
+    
+    
+    function twelvePM() {
+      var input_textarea4 = document.querySelector("#comment12pm");
+      var output_div4 = document.querySelector("#comment12pm");
+      var save_button4 = document.querySelector("#button12pm");
+    
+      save_button4.addEventListener("click", updateOutput4);
+    
+      output_div4.textContent = localStorage.getItem("content4");
+      input_textarea4.value = localStorage.getItem("content4");
+    
+      function updateOutput4() {
+        localStorage.setItem("content4", input_textarea4.value);
+    
+        output_div4.textContent = input_textarea4.value;
+      }
+    }
+    
+    
+    function onePM() {
+      var input_textarea5 = document.querySelector("#comment1pm");
+      var output_div5 = document.querySelector("#comment1pm");
+      var save_button5 = document.querySelector("#button1pm");
+    
+      save_button5.addEventListener("click", updateOutput5);
+    
+      output_div5.textContent = localStorage.getItem("content5");
+      input_textarea5.value = localStorage.getItem("content5");
+    
+      function updateOutput5() {
+        localStorage.setItem("content5", input_textarea5.value);
+    
+        output_div5.textContent = input_textarea5.value;
+      }
+    }
+    
+    
+    function twoPM() {
+      var input_textarea6 = document.querySelector("#comment2pm");
+      var output_div6 = document.querySelector("#comment2pm");
+      var save_button6 = document.querySelector("#button2pm");
+    
+      save_button6.addEventListener("click", updateOutput6);
+    
+      output_div6.textContent = localStorage.getItem("content6");
+      input_textarea6.value = localStorage.getItem("content6");
+    
+      function updateOutput6() {
+        localStorage.setItem("content6", input_textarea6.value);
+    
+        output_div6.textContent = input_textarea6.value;
+      }
+    }
+    
+    
+    function threePM() {
+      var input_textarea7 = document.querySelector("#comment3pm");
+      var output_div7 = document.querySelector("#comment3pm");
+      var save_button7 = document.querySelector("#button3pm");
+    
+      save_button7.addEventListener("click", updateOutput7);
+    
+      output_div7.textContent = localStorage.getItem("content7");
+      input_textarea7.value = localStorage.getItem("content7");
+    
+      function updateOutput7() {
+        localStorage.setItem("content7", input_textarea7.value);
+    
+        output_div7.textContent = input_textarea7.value;
+      }
+    }
+    
+    
+    function fourPM() {
+      var input_textarea8 = document.querySelector("#comment4pm");
+      var output_div8 = document.querySelector("#comment4pm");
+      var save_button8 = document.querySelector("#button4pm");
+    
+      save_button8.addEventListener("click", updateOutput8);
+    
+      output_div8.textContent = localStorage.getItem("content8");
+      input_textarea8.value = localStorage.getItem("content8");
+    
+      function updateOutput8() {
+        localStorage.setItem("content8", input_textarea8.value);
+    
+        output_div8.textContent = input_textarea8.value;
+      }
+    }
+    
+    
+    function fivePM() {
+      var input_textarea9 = document.querySelector("#comment5pm");
+      var output_div9 = document.querySelector("#comment5pm");
+      var save_button9 = document.querySelector("#button5pm");
+    
+      save_button9.addEventListener("click", updateOutput9);
+    
+      output_div9.textContent = localStorage.getItem("content9");
+      input_textarea9.value = localStorage.getItem("content9");
+    
+      function updateOutput9() {
+        localStorage.setItem("content9", input_textarea9.value);
+    
+        output_div9.textContent = input_textarea9.value;
+      }
+    }
+
+    function sixPM() {
+      var input_textarea10 = document.querySelector("#comment6pm");
+      var output_div10 = document.querySelector("#comment6pm");
+      var save_button10 = document.querySelector("#button6pm");
+    
+      save_button9.addEventListener("click", updateOutput9);
+    
+      output_div10.textContent = localStorage.getItem("content10");
+      input_textarea10.value = localStorage.getItem("content10");
+    
+      function updateOutput10() {
+        localStorage.setItem("content10", input_textarea10.value);
+    
+        output_div10.textContent = input_textarea10.value;
+      }
+    };
+
+    
+    timeTracker();
+
+
+
+    
+  })
